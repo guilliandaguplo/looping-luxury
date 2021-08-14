e45b313f1e7a50013560fac141317de6b0e0e391
Guillian Daguplo <gd80206@gmail.com>
2021-08-14 12:23:53 -0700

    Added Namebank and Item4Sale, currently working on customer factory

--- problem/customer.h
+++ problem/customer.h
@@ -2,6 +2,7 @@
 #define CUSTOMER_H
 // Include Headers
 #include <vector>
+#include <iostream>
 
 // Customer Class Here
 
@@ -9,29 +10,28 @@ class Customer {
   public:
     //Add constructor(s)
     Customer() : Customer("Michael Jackson"){}
-    Customer(std::string name) : name_(name) {}
-    Customer(std::string name, std::vector<std::string> items) : name_(name) {}
+    Customer(const std::string &name) : customerName(name) {}
+    Customer(const std::string &name, std::vector<std::string> items) : customerName(name) { copyCart(items); }
 
     //Getters and Setters for name
-    void SetName(std::string name) { customerName = name; }
+    void SetName(const std::string &name) { customerName = name; }
     std::string GetName() { return customerName; }
 
     //Getters and Setters for personalizedMessage
-    void SetCustomerMessage (std::string name) { customerMessage = name; }
+    void SetCustomerMessage (const std::string &name) { customerMessage = name; }
     std::string GetCustomerMessage() { return customerMessage; }
 
     //Add cart passed to constructor to customerCart
-    void copyCart(std::vector<string> items) {
+    void copyCart(std::vector<std::string> items) {
       for (int i = 0; i < items.size(); i++) {
         customerCart.push_back(items[i]);
         std::cout << customerCart[i] << " Copied!\n";
       }
-
     }
   private:
     std::string customerMessage, customerName;
-    std::vector<string>customerCart;
-}
+    std::vector<std::string>customerCart;
+};
 
 
 
--- problem/main.cc
+++ problem/main.cc
@@ -1,1 +1,29 @@
-## CPP audit suggests naming the driver main.cc
+#include "customer.h"
+#include <vector>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//Will create a random vector of customers
+std::vector<Customer> customerFactory();
+//Will just cout personalizedMessage of each
+void sendEmail(std::string message);
+//Get Random Customer name from namebank
+std::string randomCustomerName();
+//Get Random Luxury Item from items4sale
+std::vector<std::string> randomItem();
+
+int main() {
+
+
+ return 0;
+
+
+std::vector<Customer> customerFactory() {
+  srand(time(NULL));
+  //just Create 1 for right now
+  for (size_t i = 0; i < 1; i++) {
+    /* code */
+  }
+}
